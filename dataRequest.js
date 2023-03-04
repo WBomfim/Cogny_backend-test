3b76c51af0308ea4468a2dfe1ae733428906c53b
WBomfim <willianbomfim@hotmail.com>
2023-03-04 11:28:00 -0300

    feat: Criado a função getData - Função responsável por realizar a requisição a API externa e retornar os dados da população do USA

---
+++ dataRequest.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const URL = "https://datausa.io/api/data?drilldowns=Nation&measures=Population";
+
+const getData = async () => {
+  const { data: { data } } = await axios.get(URL);
+  return data;
+}
+
+module.exports = {
+  getData
+}
